f95d2cf347f22b88dec7261ce958c83e24fd0d60
DrQuadCore <kyhoon@gmail.com>
2019-06-03 17:28:32 +0900

    Create array_sum.c

---
+++ src/array_sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define N 10
+
+int main()
+{
+    int i;
+    int a[N];
+    for (i = 0; i < N; i++)
+        a[i] = 1;             /* initializes a */
+
+    for(i = 0; i < N; i++)
+        scanf("%d", &a[i]);   /* reads data into a */
+
+    for (i = 0; i < N; i++)
+        sum += a[i]; 		/* sums the elements of a */
+
+    return 0;
+}
