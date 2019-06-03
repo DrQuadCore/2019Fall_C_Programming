bfce199ed1d61ca2c85aa33226591c5f6760566e
DrQuadCore <kyhoon@gmail.com>
2019-06-03 17:36:27 +0900

    Create array_char_example.c

---
+++ src/array_char_example.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    //char name1[5] = "Brown";
+    //char name2[4] = {'A', 'n', 'n', 'a'};
+    //char name3[6] = {'D', 'a', 'r', 'w', 'i', 'n'};
+    
+    char name1[6] = "Brown";
+    char name2[5] = {'A', 'n', 'n', 'a'};
+    char name3[7] = {'D', 'a', 'r', 'w', 'i', 'n'};
+
+    printf("names: %s, %s, %s\n", name1, name2, name3);
+}
