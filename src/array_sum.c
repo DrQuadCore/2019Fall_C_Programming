db44fa4dfd0cf7f47eda512be6dc3ecf5217dbd7
DrQuadCore <kyhoon@gmail.com>
2019-06-03 17:29:08 +0900

    Update array_sum.c

--- src/array_sum.c
+++ src/array_sum.c
@@ -5,13 +5,14 @@ int main()
 {
     int i;
     int a[N];
-    for (i = 0; i < N; i++)
+	for (i = 0; i < N; i++)
         a[i] = 1;             /* initializes a */
 
     for(i = 0; i < N; i++)
         scanf("%d", &a[i]);   /* reads data into a */
 
-    for (i = 0; i < N; i++)
+    int sum = 0;
+	for (i = 0; i < N; i++)
         sum += a[i]; 		/* sums the elements of a */
 
     return 0;
