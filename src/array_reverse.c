1bf7a519d28e55782a0c9e284cc3a4115d527877
DrQuadCore <kyhoon@gmail.com>
2019-06-04 13:04:27 +0900

    Update array_reverse.c

--- src/array_reverse.c
+++ src/array_reverse.c
@@ -5,13 +5,10 @@ int main()
 {
     int a[N], i;
 	for (i = 0; i < N; i++)
-	  a[i] = 1;             /* initializes a */
-
-	for (i = 0; i < N; i++)
-	  scanf("%d", &a[i]);   /* reads data into a */
-
-    for (i = 9; i >= 0; i--)
-        printf("%d ", a[i]);
+		a[i] = i;             /* initializes a with its index*/
+	
+	for (i = N-1; i >= 0; i--)
+		printf("%d", &a[i]);	/* print each element of a */
 
     printf("\n");
     return 0;
